1d5442c1b113934bf7685cdcf1cee13b49490f4a
HyperRush Network <hyperrushnetwork@gmail.com>
2026-05-05 02:14:30 +0200

    Initial commit

---
+++ sw.js
@@ -0,0 +1,70 @@
+try {
+  importScripts("https://storage.googleapis.com/workbox-cdn/releases/7.0.0/workbox-sw.js");
+} catch (e) {
+  importScripts("./lib/workbox-sw.js");
+}
+const CACHE_VERSION = "1.0";
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      const keys = await caches.keys();
+      await Promise.all(
+        keys.map((key) => {
+          if (!key.includes(CACHE_VERSION)) {
+            return caches.delete(key);
+          }
+        })
+      );
+      await self.clients.claim();
+    })()
+  );
+});
+if (self.workbox) {
+  workbox.core.setCacheNameDetails({
+    prefix: "eaglercraft",
+    suffix: CACHE_VERSION,
+    precache: "precache",
+    runtime: "runtime",
+  });
+  workbox.core.skipWaiting();
+  workbox.core.clientsClaim();
+  workbox.precaching.precacheAndRoute([]);
+  workbox.routing.registerRoute(
+    ({ request }) => request.mode === "navigate",
+    async (params) => {
+      try {
+        return await fetch(params.request);
+      } catch (e) {
+        return caches.match("index.html");
+      }
+    }
+  );
+  workbox.routing.registerRoute(
+    ({ request }) =>
+      request.destination === "script" || request.destination === "style",
+    async (params) => fetch(params.request)
+  );
+  workbox.routing.registerRoute(
+    ({ url }) => url.pathname.endsWith(".json"),
+    async (params) => fetch(params.request)
+  );
+  workbox.routing.registerRoute(
+    ({ url }) => url.pathname.endsWith(".seg"),
+    new workbox.strategies.CacheFirst({
+      cacheName: "eaglercraft-segments",
+      plugins: [
+        new workbox.cacheableResponse.CacheableResponsePlugin({
+          statuses: [0, 200],
+        }),
+        new workbox.rangeRequests.RangeRequestsPlugin(),
+        new workbox.expiration.ExpirationPlugin({
+          maxAgeSeconds: 7 * 24 * 60 * 60,
+          maxEntries: 20,
+        }),
+      ],
+    })
+  );
+}
